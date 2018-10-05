64767a09c3593057ec8ac8ca6b2c422c8d936255
kevin <ulrichkev2017@gmail.com>
2018-10-05 17:05:36 +0100

    first

--- serveur.js
+++ serveur.js
@@ -1,41 +1,40 @@
 /* eslint-disable */
-var http = require('http');
 var mysql = require('mysql');
 var fs = require('fs');
 var path = require('path');
+var path = require('path');
+var express = require('express');
+var app = express();
 
-var server = http.createServer((req, res) => {
-    var chaine = '';
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
+function connector() {
     var connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
         database: "agenda"
     });
-    var sql_request = 'select * from user';
-    connection.query(sql_request, (error, result, col) => {
-        if (error) {
-            console.log(error);
-            connection.end();
+    return connection;
+}
+
+var db = connector();
+var chaine = '';
+
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM user', (err, data) => {
+        if (err) {
+            console.log(err);
             return;
-        }
-        if (result.length > 0) {
-            for (let val of result) {
-                chaine += JSON.stringify(val);
+        } else {
+            for (let val of data) {
+                chaine += JSON.stringify(data);
             }
-        } else chaine = 'vide';
-        if (req.url == '/') {
-            fs.readFile(path.join(__dirname, 'vues/connexion.html'), (err, data) => {
-                if (err)
-                    res.end(err);
-                else res.end(data);
-            });
         }
-        if (req.url == '/subcribe');
-        connection.end();
     });
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(path.join(__dirname, 'vues/connexion.html'));
+}).get('*', (req, res) => {
+    res.status(404);
+    res.sendFile(path.join(__dirname, 'vues/404.html'));
 });
-server.listen(8080, '192.168.173.1');
+
+app.listen(8080);
